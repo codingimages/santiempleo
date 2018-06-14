18a2804d958af472b1827441d44ff3bc257880c0
Reynaldo Navedo <rnavedojr@outlook.com>
2018-06-14 17:09:20 -0400

    Available time array

--- partials/ajax.php
+++ partials/ajax.php
@@ -5,13 +5,13 @@ require_once 'db.php';
 $output = '';
 
 $dayTime = [
-    'Monday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM'),
-    'Tuesday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM'),
-    'Wednesday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM'),
-    'Thursday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM'),
-    'Friday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM'),
-    'Saturday' => array('9 AM', '10 AM', '11 AM', '12 PM', '1 PM'),
-    'Sunday' => 'not available'
+    'Monday' => array('8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Tuesday' => array('8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Wednesday' => array('8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Thursday' => array('8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Friday' => array('8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Saturday' => array('10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
+    'Sunday' => array('12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',),
 ];
 
 if(isset($_POST['date'])) {
